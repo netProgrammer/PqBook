ef216d5be57c4d4887deec4929d17bde6c9720e1
netProgrammer <vova@programmer.net>
2015-09-06 00:02:43 +0300

    server add create

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Book struct  {
@@ -32,7 +33,46 @@ func init()  {
 func main()  {
 	http.HandleFunc("/books", booksIndex)
 	http.HandleFunc("/books/show", booksShow)
+	http.HandleFunc("/books/create", booksCreate)
 	http.ListenAndServe(":3000", nil)
+}
+
+func booksCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	title := r.FormValue("title")
+	author := r.FormValue("author")
+
+	if isbn == "" || title == "" || author == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	price, err := strconv.ParseFloat(r.FormValue("price"), 32)
+
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	result, err := db.Exec("INSERT INTO books VALUES($1, $2, $3, $4)", isbn, title, author, price)
+
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "Book %s created successfully (%d row affected)\n", isbn, rowsAffected)
 }
 
 func booksShow(w http.ResponseWriter, r *http.Request) {
